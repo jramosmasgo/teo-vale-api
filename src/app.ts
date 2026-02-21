3f814b8deefe756686abb2aa64deedc63ea1999e
jpramosmasgo <ramosmasgo@gmail.com>
2026-02-20 22:37:06 -0500

    fix: fix errors logs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import routes from './routes';
 import { logErrorMiddleware } from './middlewares/error.middleware';
 
-const app: Application = express();
+const app = express();
 
 // Middlewares
 app.use(express.json());
@@ -12,12 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
-
 // Routes
 app.use('/api', routes);
 
 // Health check
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req, res) => {
   res.json({ message: 'Welcome to Teo Vale Backend API' });
 });
 
