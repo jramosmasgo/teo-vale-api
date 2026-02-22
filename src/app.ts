4436116e28fb6dedf59c8072547b0cdc656bd02c
jpramosmasgo <ramosmasgo@gmail.com>
2026-02-21 19:25:48 -0500

    feat: add cors 2

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://teo-vale-admin.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
